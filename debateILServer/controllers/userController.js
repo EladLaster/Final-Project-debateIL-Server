b8c625a2cc52a4edc4a513d395ed5d4961f32fbd
LiorKirshner <liorkk7@gmail.com>
2025-09-21 16:57:15 +0300

    Fix production authentication - dynamic cookie settings

--- debateILServer/controllers/userController.js
+++ debateILServer/controllers/userController.js
@@ -32,8 +32,8 @@ async function login(req, res, next) {
 
     res.cookie("token", result.token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production", // Only secure in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Different sameSite for dev/prod
       maxAge: 24 * 60 * 60 * 1000, // 24h
       path: "/",
     });
