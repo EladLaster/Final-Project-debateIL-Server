1b627b8944ae2a065faa5597c5e1bba8a10d151a
LiorKirshner <liorkk7@gmail.com>
2025-09-24 14:55:15 +0300

    !

--- debateILServer/server.js
+++ debateILServer/server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(
   cors({
-    origin: true, // Allow all origins for now
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://final-project-debateil-client.onrender.com"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
